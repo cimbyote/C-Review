8dca9b185fb13da02db08ffb60e49af671d70716
Nick Gutierrez <cimbyote@gmail.com>
2022-04-04 11:47:29 -0500

    Update SimpleAddingScript.cpp
    following exercises from book

--- Testing/SimpleAddingScript.cpp
+++ Testing/SimpleAddingScript.cpp
@@ -5,31 +5,35 @@ using std::cout;
 using std::cin;
 using std::endl;
 
+//I don't like cout... I prefer printf, but input is a lot nicer...
+//Just hate the double cin.ignore() thing
+
 
 	void SimpleAddingScript::SimpleAdding()
 	{
 		double sum = 0;
 		double inNum = 1;
+		int count = 0;
 		cout << "Enter a list of numbers, termindated with a 0 ";
 		//cout << "or non-digit." << endl << "Enter: ";
 		//endl is a new line terminator. not a fan...
 		cout << "or non-digit.";
 		cout << " Enter: ";
 
-		while (inNum != 0)
+		while ((inNum != 0) && cin)
 		{
 			cin >> inNum;
-			if (!cin)
-			{
-				inNum = 0;
-			}
 			sum = sum + inNum;
+			count++;
 		}
-		cout << "You added up " << inNum << " numbers." << endl;
-		cout << "Total is: " << sum << endl;
-		//cout << "Press enter to exit";
 
-		//cin.ignore();
-		//cin.ignore();
-		system("Pause"); //can use this instead of teh extra cout and the cin.ignore() cin.ignore()
+		cout << "You added up " << count - 1 << " numbers." << endl;
+		cout << "Total is: " << sum << endl;
+
+
+		cout << "Press enter to exit";
+
+		cin.ignore();
+		cin.ignore();
+		//system("Pause"); //can use this instead of teh extra cout and the cin.ignore() cin.ignore()
 	}
